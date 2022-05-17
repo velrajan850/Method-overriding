3afce88d411a8b28015a3c38a71d60d418bdefb7
velrajan850 <105338994+velrajan850@users.noreply.github.com>
2022-05-17 09:36:05 +0530

    Add files via upload

---
+++ bus.java
@@ -0,0 +1,15 @@
+package velu.java;
+
+public class bus extends car {
+	
+
+	public void bus() {
+		System.out.print("bus is running safetly"); }
+
+		// TODO Auto-generated method stub
+		
+	}
+	
+			
+
+
